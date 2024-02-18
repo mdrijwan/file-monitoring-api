2fb35f3306535b3a3490b35516c1a245c97e697b
Md Rijwan Razzaq Matin <mrrmmonty@gmail.com>
2024-02-18 19:35:39 +0800

    refactor: refactoring upload function

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,55 +1,71 @@
-import { v4 as uuidv4 } from 'uuid'
+import { APIGatewayProxyEvent } from 'aws-lambda'
 import { parse } from 'lambda-multipart-parser'
+import { ErrorType, StatusCode } from '../helpers/enums'
+import {
+  fileInfoModel,
+  s3DataModel,
+  s3ParamsModel,
+  userInfoModel,
+} from '../helpers/model'
 import {
   createData,
   formatResponse,
   limitCheck,
+  measureBinarySize,
   s3Upload,
 } from '../helpers/util'
-import { ErrorType, StatusCode } from '../helpers/enums'
-import {
-  s3ParamsModel,
-  userInfoModel,
-  fileInfoModel,
-  s3DataModel,
-} from '../helpers/model'
-import { APIGatewayProxyEvent } from 'aws-lambda'
 
 export const uploadFile = async (event: APIGatewayProxyEvent) => {
   try {
-    const userId = process.env.IS_OFFLINE
-      ? event.headers.id
-      : event.requestContext.authorizer.claims.sub
+    let folder: string = ''
+    let userId, userName, userEmail
 
-    const userName = process.env.IS_OFFLINE
-      ? event.headers.name
-      : event.requestContext.authorizer.claims.name
+    if (event.requestContext.authorizer) {
+      userId = process.env.IS_OFFLINE
+        ? event.headers.id
+        : event.requestContext.authorizer.claims.sub
 
-    const userEmail = process.env.IS_OFFLINE
-      ? event.headers.email
-      : event.requestContext.authorizer.claims.email
+      userName = process.env.IS_OFFLINE
+        ? event.headers.name
+        : event.requestContext.authorizer.claims.name
 
-    if (!userEmail) {
+      userEmail = process.env.IS_OFFLINE
+        ? event.headers.email
+        : event.requestContext.authorizer.claims.email
+    }
+
+    if (userEmail === undefined) {
       return formatResponse(StatusCode.ERROR, ErrorType.AUTH)
     }
 
     const sizeLimit = await limitCheck(userEmail)
 
-    if (sizeLimit > process.env.LIMIT) {
+    if (sizeLimit > parseInt(process.env.SIZELIMIT as string)) {
       return formatResponse(StatusCode.OVERLIMIT, ErrorType.SIZE)
     }
 
     const result = await parse(event)
 
     if (result.files.length > 1) {
-      return formatResponse(StatusCode.FORBIDDEN, ErrorType.FILE)
+      return formatResponse(StatusCode.FORBIDDEN, ErrorType.UPLOAD)
     }
 
     const docFile = result.files[0]
 
+    if (
+      measureBinarySize(docFile.content) >
+      parseInt(process.env.FILELIMIT as string)
+    ) {
+      return formatResponse(StatusCode.OVERLIMIT, ErrorType.FILE)
+    }
+
+    if (userName !== undefined) {
+      folder = userName.replace(/ /g, '-')
+    }
+
     const docParam: s3ParamsModel = {
       Bucket: process.env.UPLOAD as string,
-      Key: `images/${userName.replace(/ /g, '-')}/${
+      Key: `images/${folder}/${
         docFile.fieldname
       }-${new Date().toISOString().replace('.', '-')}-${docFile.filename
         .split(' ')
@@ -58,40 +74,41 @@ export const uploadFile = async (event: APIGatewayProxyEvent) => {
       ContentType: docFile.contentType,
     }
 
-    const s3Info = await s3Upload(docParam)
+    try {
+      const s3Info = await s3Upload(docParam)
 
-    const userInfo: userInfoModel = {
-      userId: userId,
-      userName: userName,
-      userEmail: userEmail,
+      const userInfo: userInfoModel = {
+        userId,
+        userName,
+        userEmail,
+      }
+
+      const fileInfo: fileInfoModel = {
+        fileName: docFile.fieldname,
+        fileSize: s3Info.size,
+        byteSize: s3Info.bytes,
+        fileType: s3Info.type,
+        entityTag: s3Info.eTag,
+        s3Uri: s3Info.location,
+      }
+
+      const item: s3DataModel = {
+        ...userInfo,
+        ...fileInfo,
+      }
+
+      const inputData = await createData(item)
+
+      if (!inputData) {
+        return formatResponse(StatusCode.ERROR, ErrorType.DATA)
+      }
+
+      return formatResponse(StatusCode.SUCCESS, item)
+    } catch (error) {
+      console.error(error)
+
+      return formatResponse(StatusCode.ERROR, ErrorType.S3ERROR)
     }
-
-    const fileInfo: fileInfoModel = {
-      fileName: docFile.fieldname,
-      fileSize: s3Info.size,
-      byteSize: s3Info.bytes,
-      fileType: s3Info.type,
-      entityTag: s3Info.eTag,
-      s3Uri: s3Info.location,
-    }
-
-    const data: s3DataModel = {
-      ...userInfo,
-      ...fileInfo,
-    }
-
-    const item = {
-      fileId: uuidv4(),
-      ...data,
-    }
-
-    const inputData = await createData(item)
-
-    if (!inputData) {
-      return formatResponse(StatusCode.ERROR, ErrorType.DATA)
-    }
-
-    return formatResponse(StatusCode.SUCCESS, data)
   } catch (error) {
     console.error(error)
 
